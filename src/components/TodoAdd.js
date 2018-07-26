c3ac30ce597d165bbd953657d47d26795d33bd90
Tian Zhang <tizhang@CNtizhang.local>
2018-07-26 20:42:56 +0800

    add validate
    Signed-off-by: Tian Zhang <tizhang@CNtizhang.local>

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -3,24 +3,41 @@ import {connect} from 'react-redux';
 import * as actions from '../actions'
 import { Field, reduxForm, formValueSelector } from 'redux-form'
 
+const required = value => {
+  return (value ? undefined : 'Required')}
+const alphaNumeric = value =>
+  value && /[^a-zA-Z0-9 ]/i.test(value)
+    ? 'Only alphanumeric characters'
+    : undefined
+
+const renderInput = ({input, type, meta: { touched, error, warning }}) => (
+  <div>
+    <input className="form-control" {...input} type={type} />
+    {touched &&
+    ((error && <span style={{color: 'red'}}>{error}</span>) ||
+      (warning && <span style={{color: 'orange'}}>{warning}</span>))}
+  </div>
+)
+
 class TodoAdd extends PureComponent {
 
   render() {
-    const {handleSubmit, pristine, reset, submitting} = this.props;
+    const {handleSubmit, submitting} = this.props;
 
     return (
       <div className="row mt-5">
         <form onSubmit={handleSubmit} className="row col-md-12">
           <div className="col-md-10">
             <Field
-              className="form-control"
               name="item"
-              component="input"
+              component={renderInput}
               type="text"
+              validate={[required]}
+              warn={alphaNumeric}
             />
           </div>
           <div className="col-md-2">
-            <button className="btn btn-primary" type="submit">
+            <button className="btn btn-primary" type="submit" disabled={submitting}>
               Add
             </button>
           </div>
